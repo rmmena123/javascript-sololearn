74f18823584ad688d03740134c8077134f2d610a
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2021-10-23 16:08:56 -0300

    Código if

---
+++ 3 - Conditionals and Loops/14 - If/index.js
@@ -0,0 +1,16 @@
+var month = readLine();
+
+if(month == "August") {
+    console.log("vacation");
+}
+
+// -----------------------------
+
+function main() {
+    var day = readLine();
+    var rules = 'Obey the rules';
+
+    if(day == "Sunday") {
+        console.log(rules);
+    }
+}
